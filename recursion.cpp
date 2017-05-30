732c5e5fdcb8d683b2692f87f6b82815bb6034c4
Sridhar Venkat <sridharvenkat@Sridhars-Mac-mini-11349.local>
2017-05-30 14:19:36 -0400

    Initial Commit

---
+++ recursion.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  Recursion
+//
+//  Created by Sridhar Venkat on 5/22/17.
+//  Copyright © 2017 Sridhar Venkat. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+#define RECURSION 1
+#define NORMAL 2
+
+using namespace std;
+
+long sum_recursion(long currentNumber, long sum) {
+    if(currentNumber == 1) {
+        return sum;
+    } else {
+        return sum_recursion(currentNumber - 1, sum+currentNumber);
+    }
+}
+
+long sum_normal(long currentNumber) {
+    long sum = 0;
+    while(currentNumber >= 1) {
+        sum = sum + currentNumber;
+        currentNumber--;
+    }
+    return sum;
+}
+
+long sum(long number, int style) {
+    if (style == RECURSION)
+        return sum_recursion(number, 1);
+    else
+        return sum_normal(number);
+}
+
+int main(int argc, const char * argv[]) {
+
+    cout << sum(10001, RECURSION) << endl;
+    cout << sum(10001, NORMAL) << endl;
+    return 0;
+}
